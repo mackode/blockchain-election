1e050bbc43673879cfbd7465f522dbcaa010f5d7
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-30 21:12:53 +0200

    Voting test

--- test/election.js
+++ test/election.js
@@ -15,16 +15,32 @@ contract("Election", function (accounts) {
         return Election.deployed().then(function(instance) {
             electionInstance = instance;
             return electionInstance.candidates(1);
-        }).then(function(candidate ) {
+        }).then(function(candidate) {
             assert.equal(candidate[0], 1, "correct id");
             assert.equal(candidate[1], "Candidate 1", "correct name");
             assert.equal(candidate[2], 0, "correct votes count");
             return electionInstance.candidates(2);
-        }).then(function(candidate ) {
+        }).then(function(candidate) {
             assert.equal(candidate[0], 2, "correct id");
             assert.equal(candidate[1], "Candidate 2", "correct name");
             assert.equal(candidate[2], 0, "correct votes count");
             return electionInstance.candidates(2);
+        });
+    });
+
+    it("voting", function() {
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, { from: accounts[0] });
+        }).then(function(receipt) {
+            return electionInstance.voters(accounts[0]);
+        }).then(function(voted) {
+            assert(voted, "voter marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "votes incremented");
         });
     });
 
