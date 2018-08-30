ccd932fec1df9e947f63aae27ac5c72ab6b9c576
Bartłomiej Makowski <biuro@mackode.pl>
2018-08-30 19:45:33 +0200

    New Election contract with mapping

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    // Constructor
-    constructor() public {
-        candidate = "Candidate 1";
+    // Store candidates
+    // Fetch candidate
+    mapping(uint => Candidate) public candidates;
+
+    // Store candidates count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
